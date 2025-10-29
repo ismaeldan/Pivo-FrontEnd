46216f351bd0f86eab61a41f3a040cf106efee1f
Ismael <ismael.dantas@hotmail.com>
2025-10-29 18:27:18 -0300

    Adicionar a Logo à Página de Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import styles from './Login.module.css';
+import Image from 'next/image';
 
 export default function LoginPage() {
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginBox}>
+
+        <Image 
+          src="Pivo-logo.svg"
+          alt="Pivô Logo"
+          width={200}
+          height={150}
+          className={styles.logo}
+        />
+
         <h1>Pivô Board</h1>
-        <p>Acesse sua conta</p>
+        <p className={styles.p}>Acesse sua conta</p>
         {/* Formulário de Login virá aqui */}
         <input type="text" placeholder="Email" className={styles.input} />
         <input type="password" placeholder="Senha" className={styles.input} />
